3221b7264cef83e361746cb6be0991fff03db610
Diogo Cercilio <diogocercilio@gmail.com>
2019-08-21 01:46:55 -0300

    Ajusta senha para o meu banco

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.0.0.1',
 	'username' => 'root',
-	'password' => 'secret',
+	'password' => 'senha123',
 	'database' => 'beedoo_dev_test',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
